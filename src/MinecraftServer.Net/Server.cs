0c009f40e5a9f86c2515065450fdf506608a4f45
Kelly Elton <its.the.doc@gmail.com>
2017-06-15 00:04:14 -0400

    Initial

---
+++ src/MinecraftServer.Net/Server.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MinecraftServer
+{
+    public class Server : IDisposable {
+        private readonly IServerProcessWrapper _serverWrapper;
+
+        public Server(IServerProcessWrapper serverWrapper) {
+            _serverWrapper = serverWrapper;
+        }
+
+        public void Dispose() {
+            _serverWrapper?.Dispose();
+        }
+
+        public Task Start() {
+            return _serverWrapper.Launch();
+        }
+
+        public Task Stop() {
+            return _serverWrapper.Shutdown();
+        }
+    }
+
+    public interface IServerProcessWrapper : IDisposable {
+        event EventHandler<RawInputEventArgs> RawInput;
+
+        Task Launch();
+        Task Shutdown();
+        Task Kill();
+        void Write(string rawMessage);
+    }
+
+    public class RawInputEventArgs : EventArgs
+    {
+        public string Message { get; set; }
+    }
+}
